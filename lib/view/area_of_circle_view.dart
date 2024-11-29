6b9924b4c4b42b2bb03386e96d7fc96705c4d3cd
dilasha-ghimire <dilashhaa@gmail.com>
2024-11-29 19:50:20 +0545

    Area Of Circle complete

--- lib/view/area_of_circle_view.dart
+++ lib/view/area_of_circle_view.dart
@@ -1,10 +1,128 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class AreaOfCircleView extends StatelessWidget {
+class AreaOfCircleView extends StatefulWidget {
   const AreaOfCircleView({super.key});
 
   @override
+  State<AreaOfCircleView> createState() => _AreaOfCircleViewState();
+}
+
+class _AreaOfCircleViewState extends State<AreaOfCircleView> {
+  final radiusController = TextEditingController(text: "5");
+  double area = 0;
+
+  final myKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Area of Circle',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 10,
+        backgroundColor: const Color.fromARGB(255, 106, 117, 148),
+        shadowColor: const Color.fromARGB(255, 3, 1, 22),
+        iconTheme: const IconThemeData(color: Colors.white),
+      ),
+      backgroundColor: const Color.fromARGB(255, 224, 230, 230),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: myKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 20),
+
+              // User Input Radius
+              Card(
+                elevation: 4,
+                shadowColor: Colors.black45,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 5),
+                      TextFormField(
+                        controller: radiusController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter radius',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter radius';
+                          }
+                          final double? radius = double.tryParse(value);
+                          if (radius == null || radius <= 0) {
+                            return 'Please enter a valid positive number';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 5),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // Result Card
+              Card(
+                elevation: 3,
+                color: const Color.fromARGB(158, 162, 171, 196),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  child: Text(
+                    'Area: ${area.toStringAsFixed(2)}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              // Calculate Area Button
+              ElevatedButton(
+                onPressed: () {
+                  if (myKey.currentState!.validate()) {
+                    setState(() {
+                      final double radius = double.parse(radiusController.text);
+                      area = pi * radius * radius;
+                    });
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  backgroundColor: const Color.fromARGB(179, 163, 212, 180),
+                ),
+                child: const Text(
+                  'Calculate Area',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
